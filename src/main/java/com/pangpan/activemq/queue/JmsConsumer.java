68e5dfb24c8c00e844388bee7d25e639384641f0
Easons <pp13787040125>
2020-12-03 10:09:48 +0800

    添加异步接收

--- src/main/java/com/pangpan/activemq/queue/JmsConsumer.java
+++ src/main/java/com/pangpan/activemq/queue/JmsConsumer.java
@@ -3,12 +3,13 @@ package com.pangpan.activemq.queue;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 
 public class JmsConsumer {
-    public  static  final String ACTIVEMQ_URL = "tcp://192.168.56.10:61616/";
-    public  static  final String QUEUE_NAME = "quene01";
+    public static final String ACTIVEMQ_URL = "tcp://192.168.56.10:61616/";
+    public static final String QUEUE_NAME = "quene01";
 
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, IOException {
 
         //1.创建连接和给定的地址，采用默认的用户名和密码
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
@@ -23,18 +24,37 @@ public class JmsConsumer {
         Queue queue = session.createQueue(QUEUE_NAME);
         //5.创建消息的消费者
         MessageConsumer consumer = session.createConsumer(queue);
-        while (true)
-        {
-            TextMessage receive = (TextMessage)consumer.receive(4000L);//不加时间会一直等待，加了时间4秒后停止
-            if(receive != null)
-            {
-                System.out.println("消费者接收到消息*******"+receive.getText());
-            }else{
-                break;
+//        while (true)
+//        {
+//            TextMessage receive = (TextMessage)consumer.receive(4000L);//不加时间会一直等待，加了时间4秒后停止
+//            if(receive != null)
+//            {
+//                System.out.println("消费者接收到消息*******"+receive.getText());
+//            }else{
+//                break;
+//            }
+//        }
+//        consumer.close();
+//        session.close();
+//        connection.close();
+
+        //通过监听的方式来消费消息
+        consumer.setMessageListener(message -> {
+            if (message != null && message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("消费者接收到消息*******" + textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
+
+        System.in.read();//保证控制台不灭，不停止
         consumer.close();
         session.close();
         connection.close();
+
+
     }
 }
